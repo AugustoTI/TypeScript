41cc86b070ce7f79dfe8c28a9c3bb1a4e5d45908
AugustoTI <augustocesarti2021@gmail.com>
2022-03-12 19:38:44 -0300

    adicionando o ESLint, Prettier, EditorConfig, tsconfig

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import falar from './mod';
+
+const nome = 'Augusto';
+const sobrenome = 'César';
+
+falar(nome, sobrenome);
---
+++ src/mod.ts
@@ -0,0 +1,3 @@
+export default (nome: string, sobrenome: string) => {
+  console.log(`${nome} ${sobrenome}`);
+};

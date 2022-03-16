3264108bfed1360c886b8f69a70c0ca00fccac73
AugustoTI <augustocesarti2021@gmail.com>
2022-03-16 16:53:53 -0300

    vendo "Union Types"

---
+++ src/011_union_types/011_union_types.ts
@@ -0,0 +1,6 @@
+function addOurConcat(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addOurConcat(10, 20));
